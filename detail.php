1aa4d9b87fb45cb18928a147d92976a9a4236d24
Sam-Hdez <49701649+Sam-Hdez@users.noreply.github.com>
2020-06-24 23:29:09 -0500

    Update detail.php

--- detail.php
+++ detail.php
@@ -133,6 +133,12 @@
                                         </h3>
                                     </div>
                                     <button type="submit" class="mercadopago-button" formmethod="post">Pagar</button>
+                                    <form action="/procesar-pago" method="POST">
+                                        <script
+                                        src="https://www.mercadopago.com.mx/integrations/v1/web-payment-checkout.js"
+                                        data-preference-id="<?php echo $preference->id; ?>">
+                                        </script>
+                                    </form>
                                 </div>
                             </div>
                         </div>
