d65fb45cd29dde6b363d99b4c98bf96ca097daf7
Sam-Hdez <49701649+Sam-Hdez@users.noreply.github.com>
2020-06-26 21:02:36 -0500

    Update detail.php

--- detail.php
+++ detail.php
@@ -50,12 +50,11 @@
 
         // Agrega credenciales
         MercadoPago\SDK::setAccessToken('APP_USR-8058997674329963-062418-89271e2424bb1955bc05b1d7dd0977a8-592190948');
-
-        // Crea un objeto de preferencia
-        $preference = new MercadoPago\Preference();
         $producto=$_POST["product_name"];
         $price=$_POST["price"];
         $unit=$_POST["unit"];
+        // Crea un objeto de preferencia
+        $preference = new MercadoPago\Preference();
         // Crea un ítem en la preferencia
         $item = new MercadoPago\Item();
         $item->id = "1234";
