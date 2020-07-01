ed1f3a3c5af43f2ab73ac6677698d932c49e478e
Sam-Hdez <49701649+Sam-Hdez@users.noreply.github.com>
2020-06-30 20:46:43 -0500

    Update detail.php

--- detail.php
+++ detail.php
@@ -50,8 +50,8 @@
 
         // Agrega credenciales
         MercadoPago\SDK::setAccessToken('APP_USR-8058997674329963-062418-89271e2424bb1955bc05b1d7dd0977a8-592190948');
-        $image="./assets/samsung-galaxy-s9-xxl.jpg";//$_POST['img'];
         $producto=$_POST["title"];
+        $image=$_POST['img'];
         $price=$_POST["price"];
         $unit=intval($_POST["unit"]);
         // Crea un objeto de preferencia
