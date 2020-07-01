2aab64fa48f63b8a68dfa2abee51349876d817d8
Sam-Hdez <49701649+Sam-Hdez@users.noreply.github.com>
2020-06-30 22:13:40 -0500

    Update detail.php

--- detail.php
+++ detail.php
@@ -67,9 +67,9 @@
         $item->category_id = "phones";
         $preference->items = array($item);
         $preference->external_reference = "samuel.hdez.adms@gmail.com";
-        $preference->collector_id =  intval("592190948");
+        //$preference->collector_id =  intval("592190948");
         $preference->payment_methods = array(
-            //"collector_id" => intval("592190948"),
+            "collector_id" => intval("592190948"),
             "excluded_payment_methods" => array(
               array("id" => "amex")
             ),
